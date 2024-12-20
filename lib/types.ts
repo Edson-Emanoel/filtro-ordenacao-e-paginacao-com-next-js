c07580c28dc9503759829e00420534815de5d665
Edson-Emanoel <edson.emanoelofc@gmail.com>
2024-12-20 11:03:33 -0300

    Organizei o Readme e não codigo do vídeo

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export type Order = {
+      id: Number | string;
+      customer_name: string;
+      customer_email: string;
+      order_date: string;
+      amount_in_cents: number;
+      status: string;
+      created_at: string;
+      updated_at: string;
+}
